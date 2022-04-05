7362b8eee3ec76a463b1b46611ddc21616afc0d7
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-04-05 23:07:01 +0300

    chore: initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.worker\.js$/,
+      loader: 'worker-loader',
+      options: {
+        name: 'static/[hash].worker.js',
+        publicPath: '/_next/',
+      },
+    });
 
-module.exports = nextConfig
+    // Overcome Webpack referencing `window` in chunks
+    config.output.globalObject = `(typeof self !== 'undefined' ? self : this)`;
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
