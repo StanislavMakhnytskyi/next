aecc74287f9d5c4c10b179b56aa13a36c960eba1
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-04-13 17:05:51 +0300

    chore: add usePrevious hook and tests

---
+++ hooks/use-previous/use-previous.spec.tsx
@@ -0,0 +1,29 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import { usePrevious } from './use-previous';
+
+describe('usePrevious hook', () => {
+  const setUp = () =>
+    renderHook(({ state }) => usePrevious(state), {
+      initialProps: { state: 0 },
+    });
+
+  it('should return undefined on initial render', () => {
+    const { result } = setUp();
+
+    expect(result.current).toBeUndefined();
+  });
+
+  it('should always return previous state after each update', () => {
+    const { result, rerender } = setUp();
+
+    rerender({ state: 2 });
+    expect(result.current).toBe(0);
+
+    rerender({ state: 4 });
+    expect(result.current).toBe(2);
+
+    rerender({ state: 6 });
+    expect(result.current).toBe(4);
+  });
+});
