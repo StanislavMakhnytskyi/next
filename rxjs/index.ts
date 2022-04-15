4a24dc83ce2e8d540be54f7857fcc0ff847d7e00
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-04-15 20:07:54 +0300

    chore: add from() operator array, promise, generator examples

--- rxjs/index.ts
+++ rxjs/index.ts
@@ -1,8 +1,11 @@
-import { Observable, of } from 'rxjs';
+import { from, Observable, of, take } from 'rxjs';
 
 export default () => {
   _handmade();
   _of();
+  _fromArray();
+  _fromPromise();
+  _fromGenerator();
 };
 
 function _handmade(): void {
@@ -22,5 +25,32 @@ function _handmade(): void {
 }
 
 function _of(): void {
-  of<string[]>('a', 'b', 'c').subscribe((value) => console.log(value));
+  of<string[]>('of a', 'of b', 'of c').subscribe((value) => console.log(value));
+}
+
+function _fromArray(): void {
+  from<string[]>(['from a', 'from b', 'from c']).subscribe((value) =>
+    console.log(value)
+  );
+}
+
+function _fromPromise(): void {
+  from<Promise<string>>(Promise.resolve('from resolved promise')).subscribe(
+    (value) => console.log(value)
+  );
+}
+
+function _fromGenerator(): void {
+  function* generateDoubles(seed) {
+    let i = seed;
+    while (true) {
+      yield i;
+      i = 2 * i;
+    }
+  }
+
+  const iterator = generateDoubles(2);
+  const result = from(iterator).pipe(take(10));
+
+  result.subscribe((x) => console.log(x));
 }
