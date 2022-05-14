7d568686dff8b564a1e2581b4b4cc2c0d2b3df04
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-05-14 17:17:13 +0200

    chore(algorithms): add insert method to binary search tree class

--- features/algorithms/binarySearchTree/BinarySearchTree.ts
+++ features/algorithms/binarySearchTree/BinarySearchTree.ts
@@ -8,4 +8,31 @@ export class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  public insert = (value): this => {
+    const newNode = new BinarySearchTreeNode(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    }
+    let temp = this.root;
+
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  };
 }
