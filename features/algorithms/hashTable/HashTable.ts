bfb5f1afeec2383ab2b44f2957eb61602518df2c
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-05-22 12:36:27 +0200

    chore(algorithms): add hash method to HashTable class

--- features/algorithms/hashTable/HashTable.ts
+++ features/algorithms/hashTable/HashTable.ts
@@ -6,4 +6,14 @@ export class HashTable {
   constructor(size) {
     this.dataMap = new Array(size);
   }
+
+  private hash(key: string): number {
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+    }
+
+    return hash;
+  }
 }
