499a8fa2e34f06f40a6f4a1f4a4ea4b98b6f7340
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-05-23 22:14:00 +0200

    chore(algorithms): add get method to HashTable class

--- features/algorithms/hashTable/HashTable.ts
+++ features/algorithms/hashTable/HashTable.ts
@@ -24,4 +24,18 @@ export class HashTable {
     this.dataMap[index].push([key, value]);
     return this;
   }
+
+  public get(key) {
+    let index = this.hash(key);
+
+    if (this.dataMap[index]) {
+      for (let i = 0; i < this.dataMap[index].length; i++) {
+        if (this.dataMap[index][i][0] === key) {
+          return this.dataMap[index][i][1];
+        }
+      }
+    }
+
+    return undefined;
+  }
 }
