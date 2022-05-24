5b4799f1ca79280f460482d2e8c70e8f00a041a7
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-05-24 18:46:49 +0200

    chore(algorithms): refactor function to find is there intersection between two arrays with O(n) complexity so it works with any types of arrays

--- features/algorithms/arraysIntersections/arraysIntersectionsO(n).ts
+++ features/algorithms/arraysIntersections/arraysIntersectionsO(n).ts
@@ -1,10 +1,7 @@
-export const arraysIntersections = (
-  arr1: Array<number | string>,
-  arr2: Array<number | string>
-): boolean => {
-  const keys = {};
+export const arraysIntersections = (arr1: any[], arr2: any[]): boolean => {
+  const keys = new Map();
 
-  arr1.forEach((key) => (keys[key] = true));
+  arr1.forEach((key) => keys.set(key, true));
 
-  return arr2.some((key) => keys[key] === true);
+  return arr2.some((key) => keys.get(key) === true);
 };
