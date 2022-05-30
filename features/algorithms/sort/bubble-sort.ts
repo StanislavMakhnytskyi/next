88f8e3fb69854f3bdeb6865eae4ed77bcd91618d
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-05-30 22:43:58 +0200

    chore(algorithms): add bubble sort algorithm

---
+++ features/algorithms/sort/bubble-sort.ts
@@ -0,0 +1,11 @@
+export const bubbleSort = (array: number[]) => {
+  for (let i = array.length - 1; i < 1; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+
+  return array;
+};
